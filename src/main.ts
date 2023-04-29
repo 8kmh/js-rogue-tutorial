49fdf3a2ea8a20d340934bbf62a23a1ee5d411de
Maida <maida.maxime@gmail.com>
2023-04-29 11:39:41 +0200

    Hello World ! Vite TS + rot.js

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import * as ROT from "rot-js";
+
+class Engine {
+  public static readonly WIDTH = 80;
+  public static readonly HEIGHT = 50;
+
+  display: ROT.Display;
+
+  constructor() {
+    this.display = new ROT.Display({
+      width: Engine.WIDTH,
+      height: Engine.HEIGHT,
+    });
+    const container = this.display.getContainer()!;
+    document.body.appendChild(container);
+
+    this.render();
+  }
+
+  render() {
+    const x = Engine.WIDTH / 2;
+    const y = Engine.HEIGHT / 2;
+    this.display.draw(x, y, "Hello World!", "#fff", "#000");
+  }
+}
+
+declare global {
+  interface Window {
+    engine: Engine;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.engine = new Engine();
+});

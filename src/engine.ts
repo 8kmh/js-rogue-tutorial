e96d0f598dde2788b2e0f8fc3b1983fee7137bf2
8k <maida.maxime@gmail.com>
2023-05-03 11:52:32 +0200

    Part 2 done

--- src/engine.ts
+++ src/engine.ts
@@ -1,13 +1,17 @@
 import * as ROT from "rot-js";
 
-import { handleInput, MovementAction } from "./input-handler";
+import { handleInput } from "./input-handler";
 import { Entity } from "./entity";
+import { GameMap } from "./game-map";
 
 export class Engine {
   public static readonly WIDTH = 80;
   public static readonly HEIGHT = 50;
+  public static readonly MAP_WIDTH = 80;
+  public static readonly MAP_HEIGHT = 45;
 
   display: ROT.Display;
+  gameMap: GameMap;
 
   player: Entity;
   entities: Entity[];
@@ -24,6 +28,12 @@ export class Engine {
     const container = this.display.getContainer()!;
     document.body.appendChild(container);
 
+    this.gameMap = new GameMap(
+      Engine.MAP_WIDTH,
+      Engine.MAP_HEIGHT,
+      this.display
+    );
+
     window.addEventListener("keydown", (event) => {
       this.update(event);
     });
@@ -35,13 +45,14 @@ export class Engine {
     this.display.clear();
     const action = handleInput(event);
 
-    if (action instanceof MovementAction) {
-      this.player.move(action.dx, action.dy);
+    if (action) {
+      action.perform(this, this.player);
     }
     this.render();
   }
 
   render() {
+    this.gameMap.render();
     this.entities.forEach((e) => {
       this.display.draw(e.x, e.y, e.char, e.fg, e.bg);
     });
